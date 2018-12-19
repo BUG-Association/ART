47b1ab5232ac5116d198b97fd8be52330c3c944b
Thomas Gamper <icicle@cg.tuwien.ac.at>
2018-12-19 16:16:48 +0100

    -[ArnNamedNodeSet code:] do not allocate placeholder entry on heap

--- Source/ClassLibraries/Scenegraph/ArnNamedNodeSet.m
+++ Source/ClassLibraries/Scenegraph/ArnNamedNodeSet.m
@@ -366,14 +366,14 @@ ARPCONCRETECLASS_DEFAULT_IMPLEMENTATION(ArnNamedNodeSet)
 
         while ( entriesToProcess-- )
         {
-            ArNamedNode  * entry = ALLOC(ArNamedNode);
+            ArNamedNode entry;
 
-            [ coder codeSymbol : & entry->name ];
-            [ coder codeSubnode: & entry->node ];
+            [ coder codeSymbol : & entry.name ];
+            [ coder codeSubnode: & entry.node ];
 
             artable_add_nn(
                 & namedNodeTable,
-                 *entry );
+                  entry );
         }
     }
     else
