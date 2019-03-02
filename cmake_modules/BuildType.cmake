6b7693a68316babd8e2892cb4998afb053bed12f
Thomas Gamper <icicle@cg.tuwien.ac.at>
2019-03-02 17:28:31 +0100

    BuildType.cmake - adjust style and formatting to other cmake modules

--- cmake_modules/BuildType.cmake
+++ cmake_modules/BuildType.cmake
@@ -1,14 +1,16 @@
-# Set a default build type if none was specified
-set(default_build_type "Release")
-if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-  set(default_build_type "Debug")
-endif()
+# Set a default build type if none was supplied to cmake
+SET(_default_build_type "Release")
 
-if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
-  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
-      STRING "Choose the type of build." FORCE)
-  # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
-    "MinSizeRel" "RelWithDebInfo")
-endif()
+# In case of git working copy, set build type to Debug
+IF (EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    SET(_default_build_type "Debug")
+ENDIF()
+
+# Now check if build type has been supplied to cmake, either directly or via a generator
+IF (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    #
+    MESSAGE(STATUS "Missing build type, setting to '${_default_build_type}'.")
+    SET(CMAKE_BUILD_TYPE "${_default_build_type}" CACHE STRING "Choose the type of build." FORCE)
+    # Support cmake-gui
+    SET_PROPERTY(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+ENDIF()
